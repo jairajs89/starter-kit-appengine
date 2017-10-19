8feb36f8486cbe965c68c4d35bfaea1cfef03e94
Jairaj <j@jairaj.org>
2017-10-19 23:11:43 +0000

    thoughts

--- api/test.py
+++ api/test.py
@@ -6,3 +6,39 @@ from model.test import Test
 class TestHandler(Handler):
     def get(self):
         self.respond('Yea.. {}'.format(len(Test.query().fetch(keys_only=True))))
+
+# @get('/test/')
+# def do_shit(request, response):
+# 	#TODO
+# 	request.url
+# 	request.headers
+# 	request.body
+# 	response.status
+# 	response.headers
+# 	response.body
+# 	
+# 	#TODO
+# 	request.params
+# 	request.cookies
+# 	#TODO: http auth
+# 	raise response.Error(404)
+# 	raise response.Redirect('https://dick.poop/')
+# 	response.status
+# 	response.cookies
+# 	response.content_type
+# 	response.headers
+# 	response.write({})
+# 
+# 
+# @cron
+# def do_the_thing():
+# 	pass
+# 
+# 
+# @task
+# def do_stuff(params):
+# 	#TODO
+# 	pass
+# q = do_stuff.Queue()
+# q.add({}, id='asdf')
+# q.flush()
